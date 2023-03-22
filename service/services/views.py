9f01b334071841e96ab1ecd6252dadee64d70d3a
yottabufer <yottabufer@gmail.com>
2023-03-22 17:27:49 +0300

    add aggregate for Sum and add Celery+redis

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch, F
+from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from clients.models import Client
 from services.models import Subscription
@@ -12,3 +12,14 @@ class SubscriptionView(ReadOnlyModelViewSet):
                  queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
     ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        response = super().list(self, request, *args, **kwargs)
+
+        response_data = {'result': response.data}
+        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response.data = response_data
+
+        return response
