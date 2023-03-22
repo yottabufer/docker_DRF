2e0d23daebdd300fcc667553de0c6fbb25637dff
yottabufer <yottabufer@gmail.com>
2023-03-22 20:27:54 +0300

    celery_singleton

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,10 +1,14 @@
+import time
+
 from celery import shared_task
+from celery_singleton import Singleton
 from django.db.models import F
 
 
-@shared_task
+@shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
+    time.sleep(10)
 
     subscription = Subscription.objects.filter(id=subscription_id).annotate(
         annotated_price=F('service__full_price') -
