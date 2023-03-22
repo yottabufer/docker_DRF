aa25f815e3f2edeeb8f9e8a24a443be006cfbcad
yottabufer <yottabufer@gmail.com>
2023-03-22 10:55:52 +0300

    fix n+1 in SerializerMethodField

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from clients.models import Client
 from services.models import Subscription
@@ -10,5 +10,5 @@ class SubscriptionView(ReadOnlyModelViewSet):
         'plan',
         Prefetch('client',
                  queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    )
+    ).annotate(price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00)
     serializer_class = SubscriptionSerializer
