fc637a58c7a1a08a2ef4684d8a7fdcbd05054a14
yottabufer <yottabufer@gmail.com>
2023-03-27 11:56:08 +0300

    add worker2 and transaction.atomic

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -9,17 +9,10 @@ from django.db.models import F
 @shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
-
     with transaction.atomic():
-
-        time.sleep(5)  # Имитация логики
-
         subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
             annotated_price=F('service__full_price') -
                             F('service__full_price') * F('plan__discount_percent') / 100.00).first()
-
-        time.sleep(20)  # Имитация логики
-
         subscription.price = subscription.annotated_price
         subscription.save()
 
@@ -28,10 +21,6 @@ def set_price(subscription_id):
 def set_comment(subscription_id):
     from services.models import Subscription
     with transaction.atomic():
-
         subscription = Subscription.objects.select_for_update().get(id=subscription_id)
-
-        time.sleep(30)  # Имитация логики
-
         subscription.comment = str(datetime.datetime.now())
         subscription.save()
